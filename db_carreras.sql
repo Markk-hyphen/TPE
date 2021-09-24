00be06c3c506b21c713200e9c00e870e9dc168c7
MarcosFSoler <marcosfsoler@gmail.com>
2021-09-24 11:59:24 -0700

    agregada db_carreras

---
+++ db_carreras.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2021 at 08:57 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_carreras`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carrera`
+--
+
+CREATE TABLE `carrera` (
+  `id_carrera` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `duracion` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `carrera`
+--
+
+INSERT INTO `carrera` (`id_carrera`, `nombre`, `duracion`) VALUES
+(1, 'Ingenieria en Sistemas', 5),
+(2, 'TUDAI', 2.5),
+(3, 'TUPAR', 2.5),
+(4, 'TUARI', 2.5),
+(5, 'Diplomatura en Inteligencia Artificial', 0.7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `materia`
+--
+
+CREATE TABLE `materia` (
+  `id_materia` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `profesor` varchar(45) NOT NULL,
+  `id_carrera` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `materia`
+--
+
+INSERT INTO `materia` (`id_materia`, `nombre`, `profesor`, `id_carrera`) VALUES
+(1, 'Algebra Lineal', 'Karina Paz', 1),
+(2, 'POO', 'Luis Berdun', 1),
+(5, 'Web1', 'Javier Dottori', 2),
+(6, 'Web2', 'Javier Romero', 2),
+(7, 'Deep learning', 'Jose Paz', 5),
+(8, 'Integracion continua', 'Roco el Barbaro', 3),
+(9, 'Procesamiento del lenguaje natural', 'Andres Dias Pace', 5),
+(10, 'Tecnologias Web', 'Pollo Lopez', 4),
+(11, 'Comunicacion de Datos', 'Hugo Curti', 1),
+(12, 'Programacion 3', 'Laura Felice', 2),
+(13, 'POO', 'Luis Berdun', 2),
+(14, 'Web1', 'Roco Miraginai', 3),
+(15, 'Web2', 'Javier Romario', 4),
+(16, 'Comunicacion de Datos', 'El pimpollo feroz', 2),
+(17, 'Programacion 3', 'Laura Felice', 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `carrera`
+--
+ALTER TABLE `carrera`
+  ADD PRIMARY KEY (`id_carrera`);
+
+--
+-- Indexes for table `materia`
+--
+ALTER TABLE `materia`
+  ADD PRIMARY KEY (`id_materia`),
+  ADD KEY `fk_carrera_materia` (`id_carrera`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `carrera`
+--
+ALTER TABLE `carrera`
+  MODIFY `id_carrera` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `materia`
+--
+ALTER TABLE `materia`
+  MODIFY `id_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `materia`
+--
+ALTER TABLE `materia`
+  ADD CONSTRAINT `fk_carrera_materia` FOREIGN KEY (`id_carrera`) REFERENCES `carrera` (`id_carrera`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
