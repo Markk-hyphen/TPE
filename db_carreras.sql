5f72fe17cdd04c4de7c3e1929004022d4e80051c
MarcosFSoler <marcosfsoler@gmail.com>
2021-10-10 23:11:27 -0700

    agregada db

--- db_carreras.sql
+++ db_carreras.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 24, 2021 at 08:57 PM
+-- Generation Time: Oct 11, 2021 at 08:09 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `carrera` (
   `id_carrera` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
   `duracion` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -42,7 +42,11 @@ INSERT INTO `carrera` (`id_carrera`, `nombre`, `duracion`) VALUES
 (2, 'TUDAI', 2.5),
 (3, 'TUPAR', 2.5),
 (4, 'TUARI', 2.5),
-(5, 'Diplomatura en Inteligencia Artificial', 0.7);
+(5, 'DUIA', 0.7),
+(6, 'Profesorado en Informatica', 4),
+(7, 'Dota2', 10),
+(11, 'DUGAR', 0.5),
+(12, 'Experiencias Digitales', 2);
 
 -- --------------------------------------------------------
 
@@ -52,7 +56,7 @@ INSERT INTO `carrera` (`id_carrera`, `nombre`, `duracion`) VALUES
 
 CREATE TABLE `materia` (
   `id_materia` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
   `profesor` varchar(45) NOT NULL,
   `id_carrera` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -64,7 +68,6 @@ CREATE TABLE `materia` (
 INSERT INTO `materia` (`id_materia`, `nombre`, `profesor`, `id_carrera`) VALUES
 (1, 'Algebra Lineal', 'Karina Paz', 1),
 (2, 'POO', 'Luis Berdun', 1),
-(5, 'Web1', 'Javier Dottori', 2),
 (6, 'Web2', 'Javier Romero', 2),
 (7, 'Deep learning', 'Jose Paz', 5),
 (8, 'Integracion continua', 'Roco el Barbaro', 3),
@@ -76,7 +79,39 @@ INSERT INTO `materia` (`id_materia`, `nombre`, `profesor`, `id_carrera`) VALUES
 (14, 'Web1', 'Roco Miraginai', 3),
 (15, 'Web2', 'Javier Romario', 4),
 (16, 'Comunicacion de Datos', 'El pimpollo feroz', 2),
-(17, 'Programacion 3', 'Laura Felice', 4);
+(17, 'Programacion 3', 'Laura Felice', 4),
+(28, 'Pickoff', 'Necro phos', 7),
+(29, 'Maps awareness', 'BSJ', 7),
+(30, 'Itemization', 'D bowie', 7),
+(31, 'Ingenieria de Software', 'Quirque', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `nombre` varchar(45) NOT NULL DEFAULT 'NOT NULL',
+  `rol` varchar(20) DEFAULT NULL,
+  `passwd` varchar(120) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`nombre`, `rol`, `passwd`, `email`) VALUES
+('krymer', 'admin', '$2y$10$dJfQnf3EyyTUzCHogH.2wu3h1oOXOMTA8dfJBxuVHfHE/AipM/1fG', 'agus.hero@hotmail.com'),
+('pollo', 'admin', '$2y$10$D6f0/ovqeRLQJ0IcmLoLT.USm9lN9JZTgbkSMR2xVeOxwn5OPli.G', 'asd@hotmail.com'),
+('rolo', 'usuario', '$2y$10$GtFUyHIlKMf1aM2hOfjn5OGKdwmc5w10h2sKqkPJ69FkM/ssv.fJ6', 'cromo@eh.com'),
+('asdq', 'usuario', '$2y$10$hAFnaKOQtxn4F5UrYg9mBOM19mLyw8BNtcXiFYCC6ztso4lkj2Nty', 'cromo@ehh.com'),
+('gigadomo', 'usuario', '$2y$10$UGri8Imc/Vk5iwfMGG57l.5M24v5ZX9ALMIF6ZNi/VSb2YO8ZJwWm', 'elperro@gmail.com'),
+('asdasd', 'usuario', '$2y$10$rsSC2WklwUvAMVQD8DFL9OoymK2vnN4dP9MZb92N3SVu9VbRCJ8.2', 'hybrid@hotmail.com'),
+('pollos', 'usuario', '$2y$10$O0.7iUYkzGkx/SY2aKPyruAMvgBvXRjFYf31dmp/G4H6C6OlF0izK', 'paky.rdsaock_@hotmail.com'),
+('prueba', 'usuario', '$2y$10$qyCGnyaZeQ2YLX2/joUta.eQ7CBR95QP/gGanU.AWiU6zYK2AS81O', 'prueba@gmail.com'),
+('Remo', 'admin', '$2y$10$uALEzHn0srLpG7UEYkKuIenzRIe3DvZMLX2G8OULNk46moOJ71llq', 'romulo@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -96,6 +131,12 @@ ALTER TABLE `materia`
   ADD KEY `fk_carrera_materia` (`id_carrera`);
 
 --
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -103,13 +144,13 @@ ALTER TABLE `materia`
 -- AUTO_INCREMENT for table `carrera`
 --
 ALTER TABLE `carrera`
-  MODIFY `id_carrera` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_carrera` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `materia`
 --
 ALTER TABLE `materia`
-  MODIFY `id_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- Constraints for dumped tables
@@ -119,10 +160,8 @@ ALTER TABLE `materia`
 -- Constraints for table `materia`
 --
 ALTER TABLE `materia`
-  ADD CONSTRAINT `fk_carrera_materia` FOREIGN KEY (`id_carrera`) REFERENCES `carrera` (`id_carrera`) ON DELETE CASCADE;
-
--- //COMPORTAMIENTO CUANDO SE BORRE UN REGISTRO PADRE
-
+  ADD CONSTRAINT `fk_carrera_materia` FOREIGN KEY (`id_carrera`) REFERENCES `carrera` (`id_carrera`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
