19e2286e9db98d6dcb2a7cbce26733ea9582d85b
MarcosFSoler <marcosfsoler@gmail.com>
2021-10-11 16:40:32 -0700

    Arreglado que puedan pasar cualquier parametro como carrera por url

--- model/CarreraModel.php
+++ model/CarreraModel.php
@@ -49,42 +49,10 @@ class CarreraModel
        return $sentencia->fetchAll(PDO::FETCH_OBJ);
     }
     
-  
-    // //INSERTAR materia            
-    // //arreglar desde controller
-    // function insertarMateria($nombre, $profesor, $carrera){
-    //     $sentencia =$this-> db->prepare( "INSERT INTO materia(nombre,profesor,id_carrera) VALUES(?,?,?)");
-    //     $sentencia ->execute(array($nombre, $profesor, $carrera));
-  
-    //     header("Location: ".BASE_URL."home");     
-    // }
-    //     //arreglar desde controller
-    // public function borrarMateria($materia_id){
-    //     $sentencia = $this->db->prepare( "DELETE FROM materia WHERE id=?");
-    //     $sentencia->execute(array($materia_id));
-    //     header("Location: ".BASE_URL."home");
-    
-    // }
-    //     //arreglar desde controller
-    // public function modificarMateria($materia_id, $nombre, $profesor, $carrera){
-    //     $sentencia =$this->db->prepare("UPDATE materia SET materia(nombre,profesor,id_carrera) VALUES(?,?,?) WHERE id = :materia_id");
-    //     $sentencia->bindParam(':materia_id', $materia_id, PDO::PARAM_STR);
-    //     $sentencia->execute(array($nombre, $profesor, $carrera));
-    //     header("Location: ".BASE_URL."home");
-           
-    //MATERIAS POR id
-    // public function getMateriaPorId($id_materia)
-    // {
-    //     $sentencia = $this->db->prepare("SELECT * FROM materia WHERE id_materia = ?");
-    //     $sentencia->execute(array($id_materia));
-    //     return $sentencia->fetch(PDO::FETCH_OBJ);
-    // }
-
-    //filtro
-    // //arreglar desde controller
-    public function filtrarCarrera($id_carrera){  
-        $sentencia =$this->db->prepare("SELECT materia.nombre, carrera.id_carrera, materia.id_materia FROM carrera INNER JOIN materia ON carrera.id_carrera = materia.id_carrera WHERE carrera.id_carrera = ?");
-        $sentencia->execute(array($id_carrera));
+    public function filtrarCarrera($id_carrera, $nombre_carrera){  
+        $sentencia =$this->db->prepare("SELECT materia.nombre, carrera.id_carrera, materia.id_materia FROM carrera INNER JOIN materia
+                                            ON carrera.id_carrera = materia.id_carrera WHERE carrera.id_carrera = ? AND carrera.nombre = ?");
+        $sentencia->execute(array($id_carrera, $nombre_carrera));
         
         return $sentencia->fetchAll(PDO::FETCH_OBJ);
     }
